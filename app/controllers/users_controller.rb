37e48ec2c4267957210ed171934b4e2e27fffaae
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-14 09:22:42 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,14 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  	else render 'new'
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+  	else 
+      render 'new'
   	end
   end
   def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	 params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
- 	test "invalid signup information" do
+ 	test "valid signup information" do
 		get signup_path
-		assert_no_difference 'User.count' do
-			post users_path, user: {name: "", email: "user@gmail.com", password: "1234",password_confirmation: "123"}
+		assert_difference 'User.count' do
+			post_via_redirect users_path, user: {name: "aaaaaaa", email: "user@gmail.com", password: "123456",password_confirmation: "123456"}
 		end
-		assert_template 'users/new'
+		assert_template 'users/show'
  	end
 end
