c91597623c8e148dae044915ab88445b70aa4383
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-17 14:26:59 +0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,15 +4,15 @@ module SessionsHelper
 	end
 
 	def current_user
-		if (user_id = session[id: user_id])
-			@current_user ||= User.find_by(id: user_id)
-		elsif (user_id = cookies.signed[:user_id])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
-				log_in user
-				@current_user = user
-			end
-		end
+	    if (user_id = session[:user_id])
+	      @current_user ||= User.find_by(id: user_id)
+	    elsif (user_id = cookies.signed[:user_id])
+	      user = User.find_by(id: user_id)
+	      if user && user.authenticated?(cookies[:remember_token])
+	        log_in user
+	        @current_user = user
+	      end
+	    end
 	end
 
 	def logged_in?
@@ -22,7 +22,7 @@ module SessionsHelper
 	def log_out
 		session.delete(:user_id)
 		@current_user = nil
-	end
+	end	
 	
 	def remember(user)
 		user.remember
@@ -41,5 +41,18 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+	def current_user?(user)
+		current_user == user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end	
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum:100, minimum:5}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password, presence: true, length: {minimum: 6}
+	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 	
 	def User.digest(string)
-	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-	                                                  BCrypt::Engine.cost
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+		                                          BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
@@ -19,10 +19,9 @@ class User < ActiveRecord::Base
 	end
 	
 	def remember
-	    self.remember_token = User.new_token
-	    update_attribute(:remember_digest, User.digest(remember_token))
-  	end
-
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 	def authenticated?(remember_token)
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
