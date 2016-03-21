bd7fa40c5744745da31583e0e4cb66902c10ff08
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-21 14:46:43 +0700

    config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,17 +63,33 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+
+ #######################
+ # config.action_mailer.delivery_method = :smtp
+ #  host = 'peaceful-savannah-78991.herokuapp.com/'
+ #  config.action_mailer.default_url_options = { host: host }
+ #  ActionMailer::Base.smtp_settings = {
+ #    :address        => 'smtp.gmail.com',
+ #    :port           => '587',
+ #    :authentication => :plain,
+ #    :user_name      => 'nanofresh.vn@gmail.com',
+ #    :password       => '12345678a@',
+ #    :domain         => 'heroku.com',
+ #    :enable_starttls_auto => true
+ #  }
+ ####################
+
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'safe-plains-31010.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                 587,
-   domain:               'example.com',
-   user_name:            'dodaihoc.abvk@gmail.com',
-   password:             '01646236194',
-   authentication:       'plain',
-   enable_starttls_auto: true  
+   :address              => 'smtp.gmail.com',
+   :port                 => '587',
+   :domain               => 'heroku.com',
+   :user_name            => 'dodaihoc.abvk@gmail.com',
+   :password             => '01646236194',
+   :authentication       => 'plain',
+   :enable_starttls_auto => true  
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
