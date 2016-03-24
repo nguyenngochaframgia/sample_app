22c3529693d64952f9751a41978099c0d5573382
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-24 14:36:57 +0700

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "please log in."
+      redirect_to login_url
+    end
+  end
 
-
-  # def mailer_set_url_options
-  #   ActionMailer::Base.default_url_options[:host] = request.host_with_port
-  # end
 end
