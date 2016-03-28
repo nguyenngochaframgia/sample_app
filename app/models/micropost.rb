0e8c7d18d4f453e6dc028029b7acf86b214f4f21
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-28 10:16:13 +0700

    edit entry

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,7 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
+  validates :title, presence: true, length: {maximum: 50}
 
   private
     # Validates the size of an uploaded picture.
